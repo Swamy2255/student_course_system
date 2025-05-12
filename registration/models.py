bbebc583c2fc4c60f820c65a995c8d2d4b317722
Swamy Gorla <swamygorla@lewisu.edu>
2025-05-12 00:48:25 -0500

    Initial commit: setup Django project and registration app

---
+++ registration/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    seats_available = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.course.title}"
